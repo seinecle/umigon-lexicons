41aa4eb5ca8b80bcce722a95dfb5a04ed6bacd17
seinecle <1244100+seinecle@users.noreply.github.com>
2023-07-06 16:05:32 +0200

    several undocumented changes

---
+++ src/main/java/net/clementlevallois/umigon/heuristics/booleanconditions/IsHashtagOrganic.java
@@ -0,0 +1,43 @@
+/*
+ * author: Cl�ment Levallois
+ */
+package net.clementlevallois.umigon.heuristics.booleanconditions;
+
+import net.clementlevallois.umigon.heuristics.tools.LoaderOfLexiconsAndConditionalExpressions;
+import net.clementlevallois.umigon.model.BooleanCondition;
+import static net.clementlevallois.umigon.model.BooleanCondition.BooleanConditionEnum.isHashtagOrganic;
+import net.clementlevallois.umigon.model.NGram;
+
+/**
+ *
+ * @author LEVALLOIS
+ */
+public class IsHashtagOrganic {
+
+    public static BooleanCondition check(boolean stripped, NGram hashtag, LoaderOfLexiconsAndConditionalExpressions lexiconsAndTheirConditionalExpressions) {
+        BooleanCondition booleanCondition = new BooleanCondition(isHashtagOrganic);
+        String ngramAsString = hashtag.getCleanedAndStrippedNgramIfCondition(stripped);
+        for (String term : lexiconsAndTheirConditionalExpressions.getMapH3().keySet()) {
+            if (term.length() < 4) {
+                continue;
+            }
+            term = term.replace(" ", "");
+            if (ngramAsString.startsWith(term)) {
+                ngramAsString = ngramAsString.replace(term, "");
+            }
+        }
+
+        for (String term : lexiconsAndTheirConditionalExpressions.getMapH9().keySet()) {
+            if (term.length() < 4) {
+                continue;
+            }
+            term = term.replace(" ", "");
+            if (ngramAsString.startsWith(term) && lexiconsAndTheirConditionalExpressions.getMapH9().get(term) != null) {
+                    booleanCondition.setTokenInvestigatedGetsMatched(Boolean.TRUE);
+                    booleanCondition.setTextFragmentMatched(hashtag);
+            }
+        }
+
+        return booleanCondition;
+    }
+}
--- src/main/java/net/clementlevallois/umigon/heuristics/tools/HashtagLevelHeuristicsVerifier.java
+++ src/main/java/net/clementlevallois/umigon/heuristics/tools/HashtagLevelHeuristicsVerifier.java
@@ -21,7 +21,7 @@ import net.clementlevallois.umigon.model.TermWithConditionalExpressions;
  */
 public class HashtagLevelHeuristicsVerifier {
 
-    public static List<ResultOneHeuristics> check(LoaderOfLexiconsAndConditionalExpressions lexiconsAndTheirConditionalExpressions, List<NGram> termsThatAreHashTag) {
+    public static List<ResultOneHeuristics> checkSentiment(LoaderOfLexiconsAndConditionalExpressions lexiconsAndTheirConditionalExpressions, List<NGram> termsThatAreHashTag) {
         List<ResultOneHeuristics> resultsHeuristics = new ArrayList();
 
         boolean stripped = false;
@@ -61,7 +61,46 @@ public class HashtagLevelHeuristicsVerifier {
             }
         }
         return resultsHeuristics;
-
     }
+    public static List<ResultOneHeuristics> checkOrganic(LoaderOfLexiconsAndConditionalExpressions lexiconsAndTheirConditionalExpressions, List<NGram> termsThatAreHashTag) {
+        List<ResultOneHeuristics> resultsHeuristics = new ArrayList();
 
+        boolean stripped = false;
+        int loopCountMax = 2;
+        for (int i = 1; i <= loopCountMax; i++) {
+            stripped = !stripped;
+            for (NGram hashtag : termsThatAreHashTag) {
+                BooleanCondition booleanCondition1 = IsHashtagPositiveSentiment.check(stripped, hashtag, lexiconsAndTheirConditionalExpressions);
+                if (booleanCondition1.getTokenInvestigatedGetsMatched()) {
+                    ResultOneHeuristics resultOneHeuristics = new ResultOneHeuristics(Category.CategoryEnum._11, hashtag);
+                    resultOneHeuristics.getBooleanConditions().add(booleanCondition1);
+                    resultsHeuristics.add(resultOneHeuristics);
+                }
+                BooleanCondition booleanCondition2 = IsHashtagNegativeSentiment.check(stripped, hashtag, lexiconsAndTheirConditionalExpressions);
+                if (booleanCondition2.getTokenInvestigatedGetsMatched()) {
+                    ResultOneHeuristics resultOneHeuristics = new ResultOneHeuristics(Category.CategoryEnum._12, hashtag);
+                    resultOneHeuristics.getBooleanConditions().add(booleanCondition2);
+                    resultsHeuristics.add(resultOneHeuristics);
+                }
+
+                BooleanCondition booleanCondition4 = IsInHashtag.check(stripped, hashtag, lexiconsAndTheirConditionalExpressions);
+                if (booleanCondition4.getTokenInvestigatedGetsMatched()) {
+                    /* highly fragile procedure:
+                - we take the term that was matched in the boolean condition
+                - to retrieve the associated conditional expression and rule
+                - so that we can add this rule to the result of the heuristics
+                This is FRAGILE because it assumes there is a numeric rule (such as "12"), not something like 12:11
+                - a non numeric rule would not break the code though, it would fall back on setting a CategoryEnum 10 to the heuristics.
+                     */
+
+                    TermWithConditionalExpressions termWithConditionalExpressions = lexiconsAndTheirConditionalExpressions.getMapH13().get(booleanCondition4.getTextFragmentMatched().getOriginalForm().toLowerCase());
+                    ResultOneHeuristics resultOneHeuristics = new ResultOneHeuristics(Category.CategoryEnum._12, booleanCondition4.getTextFragmentMatched());
+                    resultOneHeuristics.getBooleanConditions().add(booleanCondition4);
+                    resultOneHeuristics.setCategoryEnum(new Category(termWithConditionalExpressions.getRule()).getCategoryEnum());
+                    resultsHeuristics.add(resultOneHeuristics);
+                }
+            }
+        }
+        return resultsHeuristics;
+    }
 }
